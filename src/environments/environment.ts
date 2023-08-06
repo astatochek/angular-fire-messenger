b7ec1f40bd9d66b1a86860977d5c8a1257f32ab3
astatochek <71458498+astatochek@users.noreply.github.com>
2023-08-06 21:07:29 +0300

    add missing files

---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    projectId: 'angular-fire-messenger-ee413',
+    appId: '1:1009820806003:web:051184b711f11f65177306',
+    storageBucket: 'angular-fire-messenger-ee413.appspot.com',
+    apiKey: 'AIzaSyDg1lO98d2yxzevl33NMgYJ-yDNtfawHGQ',
+    authDomain: 'angular-fire-messenger-ee413.firebaseapp.com',
+    messagingSenderId: '1009820806003',
+  },
+};
